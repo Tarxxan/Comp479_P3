98aa6eb0ca5186a59469e2b0e98e05a0a3d671eb
tarxxan <cpangia881@gmail.com>
2022-11-04 15:53:39 -0400

    Added one more test

--- Project Testing/Testing Pipelines/Subproject1_testing.py
+++ Project Testing/Testing Pipelines/Subproject1_testing.py
@@ -3,14 +3,14 @@ import time
 
 from Indexers import *
 
-# # Time to create the indexer with 10000 doc pairs
-# print("10 000 Term-DocID pairs for the SPIMI inspired indexer")
-# naive, start_time = indexer()
-# print("--- %s seconds --- \n" % (time.time() - start_time))
-# print("10 000 Term-DocID pairs for the P2 indexer")
-# start_time,indp2 = indexer_p2()
-# print("--- %s seconds ---" % (time.time() - start_time))
-# start_time = (time.time())
+# Time to create the indexer with 10000 doc pairs
+print("10 000 Term-DocID pairs for the SPIMI inspired indexer")
+naive, start_time = indexer()
+print("--- %s seconds --- \n" % (time.time() - start_time))
+print("10 000 Term-DocID pairs for the P2 indexer")
+start_time,indp2 = indexer_p2()
+print("--- %s seconds ---" % (time.time() - start_time))
+start_time = (time.time())
 
 #create index from p2 just have to figure out what to import
 
